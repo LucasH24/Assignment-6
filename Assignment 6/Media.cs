6d42c613355f2ff2b93990859eae6b1fd24b6271
Lucas H <lhatfield1@my.wctc.edu>
2022-09-24 07:08:39 -0500

    Add project files.

---
+++ Assignment 6/Media.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment6;
+
+    public abstract class Media
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public abstract String Display();
+    }
---
+++ Assignment 6/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Assignment6;
+
+public class Movie : Media
+{
+    public string Genres { get; set; }
+
+    public override string Display()
+    {
+        List<string> Id = new List<string>();
+        List<string> Title = new List<string>();
+        List<string> Genre = new List<string>();
+        String filename = "movies.csv";
+        StreamReader sr = new StreamReader(filename);
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            int idx = line.IndexOf('"');
+            string[] movies = line.Split(',');
+            Id.Add(movies[0]);
+            Title.Add(movies[1]);
+            Genre.Add(movies[2]);
+        }
+        sr.Close();
+
+        for (int i = 0; i < Id.Count; i++)
+        {
+            Console.WriteLine($"Id: {Id[i]}");
+            Console.WriteLine($"Title: {Title[i]}");
+            Console.WriteLine($"Genre(s): {Genre[i]}");
+            Console.WriteLine();
+        }
+        String blank = "why do I have to return something";
+        return blank;
+
+    }
+}
